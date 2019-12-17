1519d196c3d58c1c166451331da586cd08c93246
ThePBone <thebone.main@gmail.com>
2019-12-17 18:53:49 +0100

    Updated Packages

--- builddeb.sh
+++ builddeb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Portable DEB build script; V4L_Frontend binary needs to be in working directory
-version=1.13
+version=1.14
 revision=0
 debname="viper4linux-gui_"$version-$revision
 mkdir $debname
